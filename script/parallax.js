30e65f5a7d081b687d2792e1a112a5545e71d16d
YeetbenderDamB <53065389+YeetbenderDamB@users.noreply.github.com>
2020-02-29 14:23:05 -0800

    Spring Cleaning

---
+++ script/parallax.js
@@ -0,0 +1,28 @@
+const backgrounds = document.querySelectorAll("#background");
+const header = document.querySelectorAll('#header');
+const range = 20;
+
+const calcValue = (a, b) => (a / b * range - range / 2).toFixed(1);
+
+let timeout;
+document.addEventListener('mousemove', ({
+    x,
+    y
+}) => {
+    if (timeout) {
+        window.cancelAnimationFrame(timeout);
+    }
+
+    timeout = window.requestAnimationFrame(() => {
+        const yValue = calcValue(y, window.innerHeight);
+        const xValue = calcValue(x, window.innerWidth);
+
+        [].forEach.call(header, (header) => {
+            header.style.transform = `translateX(${-xValue * 1.2}px) translateY(${-yValue * 1.2}px)`;
+        });
+
+        [].forEach.call(backgrounds, (background) => {
+            background.style.backgroundPosition = `${-xValue * .45}px ${-yValue * .45}px`;
+        });
+    });
+}, false);
--- script/shadowHover.js
+++
@@ -1,1 +0,0 @@
-$('img').hover.toggleClass('shadow');
