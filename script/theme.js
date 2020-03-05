8afaabce01e9b33421ebce3e0b54089729b7dd64
YeetbenderDamB <53065389+YeetbenderDamB@users.noreply.github.com>
2020-03-04 21:27:57 -0800

    darkBody Addition

--- script/theme.js
+++ script/theme.js
@@ -7,15 +7,15 @@ if (!theme) {
 
 function swapTheme() {
     if (theme == 'dark') {
-        $('body').removeClass('darkBody');
-        $('.navbar').removeClass('dark');
-        $('.navbar').addClass('navbar-light');
-        $('.navbar').removeClass('navbar-dark')
-        $('.card-body').removeClass('dark');
-        $('.card-header').removeClass('dark');
-        $('.card-footer').removeClass('dark');
-        $('.jumbotron').removeClass('dark');
-        $('.card').removeClass('dark');
+        $('body').addClass('darkBody');
+        $('.navbar').addClass('dark');
+        $('.navbar').removeClass('navbar-light');
+        $('.navbar').addClass('navbar-dark')
+        $('.card-body').addClass('dark');
+        $('.card-header').addClass('dark');
+        $('.card-footer').addClass('dark');
+        $('.jumbotron').addClass('dark');
+        $('.card').addClass('dark');
         Cookies.set('theme', 'light', {
             expires: 7
         });
@@ -37,15 +37,17 @@ function swapTheme() {
 
 function setTheme() {
     if (theme == 'light') {
-        $('.navbar').removeClass('dark');
-        $('.navbar').addClass('navbar-light');
-        $('.navbar').removeClass('navbar-dark')
-        $('.card-body').removeClass('dark');
-        $('.card-header').removeClass('dark');
-        $('.card-footer').removeClass('dark');
-        $('.jumbotron').removeClass('dark');
-        $('.card').removeClass('dark');
+        $('body').addClass('darkBody');
+        $('.navbar').addClass('dark');
+        $('.navbar').removeClass('navbar-light');
+        $('.navbar').addClass('navbar-dark')
+        $('.card-body').addClass('dark');
+        $('.card-header').addClass('dark');
+        $('.card-footer').addClass('dark');
+        $('.jumbotron').addClass('dark');
+        $('.card').addClass('dark');
     } else if (theme == 'dark') {
+        $('body').addClass('darkBody');
         $('.navbar').addClass('dark');
         $('.navbar').removeClass('navbar-light');
         $('.navbar').addClass('navbar-dark')
