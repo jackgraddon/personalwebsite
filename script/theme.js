e59c698f38314667e04c7da851b4464bc6a80bc8
YeetbenderDamB <53065389+YeetbenderDamB@users.noreply.github.com>
2020-03-04 19:48:16 -0800

    Testing Theme

--- script/theme.js
+++ script/theme.js
@@ -10,37 +10,11 @@ function swapTheme() {
         Cookies.set('theme', 'dark', {
             expires: 7
         });
-        // Navbar
-        $('.navbar').removeClass('navbar-light');
-        $('.navbar').removeClass('bg-light');
-        $('.navbar').addClass('navbar-dark')
-        $('.navbar').addClass('bg-dark');
-        // Body
-        $('.jumbotron').removeClass('bg-light');
-        $('.jumbotron').addClass('bg-dark');
-        $('.card-body').removeClass('bg-light');
-        $('.card-body').addClass('bg-dark');
-        $('.card-header').removeClass('bg-light');
-        $('.card-header').addClass('bg-dark');
-        $('.card-footer').removeClass('bg-light');
-        $('.card-footer').addClass('bg-dark');
+        $('html').toggleClass('dark');
     } else if (theme == 'dark') {
         Cookies.set('theme', 'light', {
             expires: 7
         });
-        // Navbar
-        $('.navbar').addClass('navbar-light');
-        $('.navbar').addClass('bg-light');
-        $('.navbar').removeClass('navbar-dark')
-        $('.navbar').removeClass('bg-dark');
-        // Body
-        $('.jumbotron').removeClass('bg-light');
-        $('.jumbotron').addClass('bg-dark');
-        $('.card-body').addClass('bg-light');
-        $('.card-body').removeClass('bg-dark');
-        $('.card-header').addClass('bg-light');
-        $('.card-header').removeClass('bg-dark');
-        $('.card-footer').addClass('bg-light');
-        $('.card-footer').removeClass('bg-dark');
+        $('html').toggleClass('dark');
     }
 }
