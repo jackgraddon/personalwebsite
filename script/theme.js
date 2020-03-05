59d169158fb595fd61f976a4f84d13feb604aa22
YeetbenderDamB <53065389+YeetbenderDamB@users.noreply.github.com>
2020-03-04 20:17:40 -0800

    Theme

--- script/theme.js
+++ script/theme.js
@@ -7,30 +7,30 @@ if (!theme) {
 
 function swapTheme() {
     if (theme != 'dark') {
-        $('.navbar').toggleClass('dark');
-        $('.navbar').toggleClass('navbar-light');
-        $('.navbar').toggleClass('navbar-dark')
-        $('.card-body').toggleClass('dark');
-        $('.card-header').toggleClass('dark');
-        $('.card-footer').toggleClass('dark');
-        $('.jumbotron').toggleClass('dark');
-        $('.card').toggleClass('dark');
+        $('.navbar').removeClass('dark');
+        $('.navbar').addClass('navbar-light');
+        $('.navbar').removeClass('navbar-dark')
+        $('.card-body').removeClass('dark');
+        $('.card-header').removeClass('dark');
+        $('.card-footer').removeClass('dark');
+        $('.jumbotron').removeClass('dark');
+        $('.card').removeClass('dark');
         setTimeout(()=> {
-            Cookies.set('theme', 'dark', {
+            Cookies.set('theme', 'light', {
                 expires: 7
             });
         }, 100)
     } else if (theme != 'light') {
-        $('.navbar').toggleClass('dark');
-        $('.navbar').toggleClass('navbar-light');
-        $('.navbar').toggleClass('navbar-dark')
-        $('.card-body').toggleClass('dark');
-        $('.card-header').toggleClass('dark');
-        $('.card-footer').toggleClass('dark');
-        $('.jumbotron').toggleClass('dark');
-        $('.card').toggleClass('dark');
+        $('.navbar').addClass('dark');
+        $('.navbar').removeClass('navbar-light');
+        $('.navbar').addClass('navbar-dark')
+        $('.card-body').addClass('dark');
+        $('.card-header').addClass('dark');
+        $('.card-footer').addClass('dark');
+        $('.jumbotron').addClass('dark');
+        $('.card').addClass('dark');
         setTimeout(()=> {
-            Cookies.set('theme', 'light', {
+            Cookies.set('theme', 'dark', {
                 expires: 7
             });
         }, 100)
