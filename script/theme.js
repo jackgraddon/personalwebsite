5322546d1c1a9a00ccb41ba1df452347b396cae4
YeetbenderDamB <53065389+YeetbenderDamB@users.noreply.github.com>
2020-03-04 19:35:20 -0800

    Working on Cookies and Theme

--- script/theme.js
+++ script/theme.js
@@ -1,12 +1,12 @@
 const theme = Cookies.get('theme');
+if (!theme) {
+    Cookies.set('theme', 'light', {
+        expires: 7
+    });
+    $('.toast').toast();
+}
 
 function swapTheme() {
-    if (!theme) {
-        Cookies.set('theme', 'light', {
-            expires: 7
-        });
-        $('.toast').toast();
-    }
     if (theme == 'light') {
         Cookies.set('theme', 'dark', {
             expires: 7
