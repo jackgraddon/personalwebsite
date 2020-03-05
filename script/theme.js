2070e842a4f443f2c4f219cdff9eba6080715687
YeetbenderDamB <53065389+YeetbenderDamB@users.noreply.github.com>
2020-03-04 20:20:20 -0800

    Theme

--- script/theme.js
+++ script/theme.js
@@ -38,7 +38,7 @@ function swapTheme() {
 }
 
 function setTheme() {
-    if (theme == 'dark') {
+    if (theme == 'light') {
         $('.navbar').removeClass('dark');
         $('.navbar').addClass('navbar-light');
         $('.navbar').removeClass('navbar-dark')
@@ -47,7 +47,7 @@ function setTheme() {
         $('.card-footer').removeClass('dark');
         $('.jumbotron').removeClass('dark');
         $('.card').removeClass('dark');
-    } else if (theme == 'light') {
+    } else if (theme == 'dark') {
         $('.navbar').addClass('dark');
         $('.navbar').removeClass('navbar-light');
         $('.navbar').addClass('navbar-dark')
